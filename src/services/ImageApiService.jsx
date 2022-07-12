07dc7298f1e6febfd7ea9b7e0037fd979f739f85
EduardSergienko <katleraster@gmail.com>
2022-07-12 19:03:46 +0300

    Update ImageApiService.jsx

--- src/services/ImageApiService.jsx
+++ src/services/ImageApiService.jsx
@@ -4,6 +4,6 @@ export async function fechImg(searchQwery, page) {
   const BASIC_URL = 'https://pixabay.com/api/';
   const KEY = '27491202-6941cbc6cc49fba95622056d0';
   return await axios.get(
-    `${BASIC_URL}?key=${KEY}&q=${searchQwery}&page=${page}&per_page=12`
+    `${BASIC_URL}?key=${KEY}&q=${searchQwery}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
   );
 }
